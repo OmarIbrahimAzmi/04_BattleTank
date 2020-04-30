596ee8dbcf39cb4d72a41d5a92bf885e1c654b4b
OmarIbrahimAzmi <omar_music101@hotmail.com>
2020-05-01 02:19:13 +0800

    BlueprintCallable()

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -4,7 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "TankAimingComponent.h"
+#include "Tank.generated.h" //  Must be the last include
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -14,6 +15,14 @@ class BATTLETANK_API ATank : public APawn
 public:	
 
 	void AimAt(FVector HitLocation);
+	
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+protected:
+
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
 
 private:
 
@@ -28,5 +37,4 @@ private:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
