8c719da96f38a485841bc34666cf4f01ead0272a
OmarIbrahimAzmi <omar_music101@hotmail.com>
2020-04-30 21:12:18 +0800

    Using LineTraceSingleByChannel() - (Slight problem, my x value when pointing to the sky is 0.008 instead of 0.000)

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,7 +15,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
@@ -24,6 +24,21 @@ public:
 
 	// Start the tank moving the barrel so that a shot would hit the crosshair intersects the world
 	void AimTowardsCrosshair();
+	
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+	
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
 
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 
 };
+
