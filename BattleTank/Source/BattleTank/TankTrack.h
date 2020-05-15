f5d04d3c6f097cdd1a30294431f87326009ac3f8
OmarIbrahimAzmi <omar_music101@hotmail.com>
2020-05-16 02:44:02 +0800

    BT54 ApplyForceAtLocation() v2

--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -19,4 +19,8 @@ public:
 	// Sets a throttle between -1 and +1.
 	UFUNCTION(BlueprintCallable, Category = Input)
 		void SetThrottle(float Throttle);
+
+	// Max force per track in newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 40000000; 
 };
