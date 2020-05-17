a7bba64570203a2030edb63c6066de22e6b1e008
OmarIbrahimAzmi <omar_music101@hotmail.com>
2020-05-17 18:35:59 +0800

    BT71 BlueprintImplementableEvent

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -1,22 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Press ST:art.
 
 #include "BattleTank.h"
 #include "Engine/World.h"
 #include "Tank.h"
+#include "TankAimingComponent.h"
 #include "TankPlayerController.h"
 
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto AimingComponent = GetControlledTank()->FindComponentByClass<UTankAimingComponent>();
+	if (AimingComponent)
 	{
-		UE_LOG(LogTemp, Error, TEXT("PlayerController is not possessing a tank!"));
+		FoundAimingComponent(AimingComponent);
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("PlayerController has possessed %s."), *(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("Player controller can't find aiming component at BeginPlay"));
 	}
 }
 
