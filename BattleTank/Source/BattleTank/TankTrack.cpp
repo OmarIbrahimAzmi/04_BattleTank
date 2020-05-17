d33c205fed7f7a2a3197c6bad90be2339019b803
OmarIbrahimAzmi <omar_music101@hotmail.com>
2020-05-18 01:19:05 +0800

    BT80 Programmatic Sideways Friction

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -3,8 +3,10 @@
 #include "BattleTank.h"
 #include "TankTrack.h"
 
-
-
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
 
 void UTankTrack::SetThrottle(float Throttle)
 {
@@ -12,4 +14,18 @@ void UTankTrack::SetThrottle(float Throttle)
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+
+	// Workout the required acceleration this frame to correct
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+
+	// Calculate and apply sideways for (F = m a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Divide by 2 because there are 2 tracks.
+	TankRoot->AddForce(CorrectionForce);
 }
--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -22,4 +22,10 @@ public:
 	// Max force per track in Newtons
 	UPROPERTY(EditDefaultsOnly)
 		float TankMaxDrivingForce = 60000000; // Assume 40 tonne tank, and 1g accelleration.
+
+private:
+
+	UTankTrack();
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
